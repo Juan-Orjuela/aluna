e8718e9a3b3726d27eceba96f0a5b37782631abf
Juan Orjuela <orjuelajuans@gmail.com>
2020-01-03 17:21:56 -0500

    Proyecto listo

--- js/interactividad.js
+++ js/interactividad.js
@@ -1,6 +1,13 @@
 $( document ).ready(function() {
+  //Home
     $( "#al-btn" ).on( "click", function() {
         $(this).toggleClass("is-active");
         $(".al-overlay").toggle();
       });
+  //Proyecto
+  $('#al-py-slider').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
 });
