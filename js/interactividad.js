fad549201d28dac17e2bd25a1dea6b3922207087
Juan Orjuela <orjuelajuans@gmail.com>
2020-01-10 13:06:20 -0500

    Animaciones 1

--- js/interactividad.js
+++ js/interactividad.js
@@ -5,9 +5,28 @@ $( document ).ready(function() {
         $(".al-overlay").toggle();
       });
   //Proyecto
+  
   $('.al-slider').slick({
     infinite: false,
     slidesToShow: 1,
     slidesToScroll: 1
   });
+
+  //Transiciones
+  $("a").on("click", redirigir);
+
+  function redirigir(e){
+    e.preventDefault();
+    var link = $(this).attr("href");
+    var linkB = link.substr(0, link.indexOf('.'));
+    
+    var delay = 1200; // time in milliseconds
+    $(".tr-overlay").addClass(linkB).show(600);
+     
+    setTimeout(function(){
+     window.location = link;
+    },delay);
+    
+   }
+
 });
